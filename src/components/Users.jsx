ecfe646a37638e2cf00a3cc743b13b5c796d7328
MasumRezaMunna <masumrezamunna2005@gmail.com>
2026-01-17 01:18:30 +0600

    save data, insertone user data in the database collection

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,38 +1,43 @@
 import React from "react";
+import { data } from "react-router";
 
 const Users = () => {
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    console.log(name, email);
 
-    const handleAddUser = e =>{
-        e.preventDefault();
-        const name = e.target.name.value;
-        const email = e.target.email.value;
-        console.log(name,email);
+    const newUser = { name, email };
 
-        const newUser = {name, email};
-
-        // save this user data to the database
-        fetch('http://localhost:3000/users',{
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(newUser)
-        })
-        .then(res => res.json())
-        .then(data =>{
-            console.log('after saving user', data)
-        })
+    // save this user data to the database
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("after saving user", data);
+      });
+    if (data.insertedId) {
+      alert("users added successfully");
+      e.target.reset();
     }
+  };
   return (
-  <div>
-    <form onSubmit={handleAddUser} action="">
+    <div>
+      <form onSubmit={handleAddUser} action="">
         <input type="text" name="name" id="" />
         <br />
         <input type="email" name="email" id="" />
         <br />
         <input type="submit" value="Add User" />
-    </form>
-  </div>
-)};
+      </form>
+    </div>
+  );
+};
 
 export default Users;
